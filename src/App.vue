3a81acbb0638d63f4c96cda90d49bef37c26ea0e
phamducbinh <phamducbinh1712000@gmail.com>
2022-10-02 20:41:58 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <QuizForm />
+  </div>
+</template>
+
+<script>
+import QuizForm from "./components/QuizForm.vue";
+export default {
+  setup() {
+    return {};
+  },
+  components: { QuizForm },
+};
+</script>
+
+<style lang="scss" scoped></style>
